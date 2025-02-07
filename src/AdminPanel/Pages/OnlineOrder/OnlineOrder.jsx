f48b4e6c0eafa645240c98ee43c535339b09b49f
KashifKhalil986 <kashifkhalil720@gmail.com>
2025-02-07 17:54:54 +0500

    Replaced order section with online & by hand order sections & enhanced By Hand Order functionality

--- src/AdminPanel/Pages/OnlineOrder/OnlineOrder.jsx
+++ src/AdminPanel/Pages/OnlineOrder/OnlineOrder.jsx
@@ -1,54 +1,55 @@
+import { Link } from "react-router-dom";
 import LeftSideBar from "../../LeftSideBar/LeftSideBar"
 import Navbar from "../../Navbar/Navbar"
-import { useSelector ,useDispatch } from 'react-redux';
-import DeleteModal from '../../Components/DeleteModal';
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { fetchOrder } from "../../Slice/OrderSlice";
+import { useSelector, useDispatch } from 'react-redux';
 import GenericTable from "../../Components/Table/GenericTable";
+import {fetchOrder} from "../../Slice/OrderSlice"
+// import OrderData from "../../../../public/Order.json"
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+import DeleteModal from '../../Components/DeleteModal';
+import { useState, useEffect } from "react";
 
 
-
-const Delever = () => {
-    const dispatch =useDispatch();
+const Order = () => {
     const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
-    const currentTheme = useSelector((state => state.theme.theme))
-    const [rows, setRowsToShow] = useState(5);
-    const [searchQuery, setSearchQuery] = useState("")
+    const [rowToShow, setRowsToShow] = useState(5);
+    const [searchQuery, setSearchQuery] = useState("");
 
-    const { data: deliverData, loading, error } = useSelector(
-        (state) => state.deliver
-      );
+
+    const currentTheme = useSelector((state => state.theme.theme))
+    const dispatch = useDispatch();
+    const { data: orderData, loading, error } = useSelector((state) => state.orders)
+
 
     const handleRowChange = (e) => {
-        setRowsToShow(parseInt(e.target.value, 10))
+        const selectedValue = parseInt(e.target.value, 10);
+        setRowsToShow(selectedValue)
     }
-
     const handleSearchQuery = (e) => {
-        setSearchQuery(e.target.value.toLoweCase())
+        setSearchQuery(e.target.value.toLowerCase());
     }
 
-    const handleEdit = ()=>{
-        console.log("edit function executed")
-    }
+    const handleDelete = () => {
+        setIsDeleteModalOpen(true);
+        // Add your delete logic here
+    };
+    const handleEdit = () => {
+        console.log("Edit Function Click")
+        // Add your delete logic here
+    };
+    useEffect(() => {
+        dispatch(fetchOrder());
 
-    const handleDelete = () =>{
-     setIsDeleteModalOpen(true)
-    }
+    }, [dispatch]);
 
+    const filterData = orderData.filter((order) => {
+        console.log(orderData)
+        return order.orderStatus.toLowerCase().includes(searchQuery) 
+         
+    })
+    const displayData = filterData.slice(0, rowToShow)
 
-   useEffect(()=>{
-    dispatch(fetchOrder())
-    console.log(fetchOrder)
-   },[dispatch])
-
-    
-   const filterData =deliverData.filter((deliver)=>{
-    console.log(deliverData)
-    return deliver.deliverStatus.toLoweCase().includes(searchQuery)
-   })
-
-   const displayData = filterData.slice(0,rows)
     return (
         <div>
 
@@ -58,7 +59,7 @@ const Delever = () => {
                 <LeftSideBar />
                 <div className='flex flex-col  lg:ml-10 w-full lg:w-[1000px] gap-3 '>
                     <div className="para ">
-                        <p className={`underline text-xl ${currentTheme === 'dark' ? 'text-white' : 'text-black'}`}>Delever Orders Details</p>
+                        <p className={`underline text-xl ${currentTheme === 'dark' ? 'text-white' : 'text-black'}`}>Online Order Details</p>
                     </div>
                     <div className="info flex flex-col lg:flex-row justify-between  items-center gap-2">
                         <div className='flex flex-col lg:flex-row gap-2 items-center w-full lg:w-[auto]'>
@@ -66,12 +67,19 @@ const Delever = () => {
                                 <span>Show:</span>
                                 <select
                                     className={`rounded-md px-4 py-1 ${currentTheme === 'dark' ? 'bg-[#404040]' : 'bg-[#F0FFF8]'} border border-gray-300 focus:outline-none focus:ring focus:ring-[#219b53]`}
+                                    value={rowToShow}
                                     onChange={handleRowChange}
-                                    value={rows}
                                 >
-                                    {[...Array(10).keys()].map(num => (
-                                        <option key={num + 1} value={num + 1}>{num + 1}</option>
-                                    ))}
+                                    <option value={1}>01</option>
+                                    <option value={2}>02</option>
+                                    <option value={3}>03</option>
+                                    <option value={4}>04</option>
+                                    <option value={5}>05</option>
+                                    <option value={6}>06</option>
+                                    <option value={7}>07</option>
+                                    <option value={8}>08</option>
+                                    <option value={9}>09</option>
+                                    <option value={10}>10</option>
                                 </select>
                             </div>
                             <div className={`flex items-center ${currentTheme === 'dark' ? 'text-white' : 'text-black'} gap-2`}>
@@ -79,7 +87,7 @@ const Delever = () => {
                                 <span >Entries :</span>
                                 <input
                                     type="text"
-                                    placeholder="Search"
+                                    placeholder="Search by Order Status"
                                     className={`rounded-md px-4 py-1 ${currentTheme === 'dark' ? 'bg-[#404040]' : 'bg-[#F0FFF8]'} border border-gray-300 focus:outline-none focus:ring focus:ring-[#219b53]`}
                                     onChange={handleSearchQuery}
                                 />
@@ -92,25 +100,30 @@ const Delever = () => {
                                 </button>
                             </Link>
 
-                            <Link to="/delivery-form">
+                            <Link to="/online-order-form">
                                 <button className={`px-4 py-2 ${currentTheme === 'dark' ? 'bg-[#404040]' : 'bg-[#F0FFF8]'} ${currentTheme === 'dark' ? 'text-white' : 'text-black'}  rounded  border`}>
-                                    Add Delivery
+                                    Add Order
                                 </button>
                             </Link>
                         </div>
 
                     </div>
                     <div className="table-container overflow-x-auto">
-                               {loading && <p>Loading...</p>}
-                               {error && <p>Error: {error}</p>}
-                               <GenericTable
-                                 headers={['SNo',  'createdAt', 'updatedAt', 'Actions']}
-                                 data={displayData}
-                                 currentTheme={currentTheme}
-                                 onEdit={handleEdit}
-                                 onDelete={handleDelete}
-                               />
-                             </div>
+
+                      
+                        {loading && <p>Loading...</p>}
+                        {error && <p>Error: {error}</p>}
+                        <GenericTable
+                            headers={['Sno', 'createdAt', 'orderStatus','paymentMethod','shippingAddress','totalAmount','updatedAt','Actions']}
+                            data={displayData}
+                            currentTheme={currentTheme}
+                            onEdit={handleEdit}
+                            onDelete={handleDelete}
+
+                        />
+
+
+                    </div>
 
                     <div className="pages flex justify-center gap-1 mt-4">
 
@@ -138,4 +151,4 @@ const Delever = () => {
     )
 }
 
-export default Delever
+export default Order
